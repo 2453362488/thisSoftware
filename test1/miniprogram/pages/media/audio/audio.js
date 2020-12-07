9dbc2062b15f5b5d52912372655db038c09a9d62
公交痴汉 <2453362488@qq.com>
2020-12-07 16:09:00 +0800

    画布，媒体，地图，视频容器等一些样例模板的提交

---
+++ test1/miniprogram/pages/media/audio/audio.js
@@ -0,0 +1,24 @@
+Page({
+  onReady: function (e) {
+    // 使用 wx.createAudioContext 获取 audio 上下文 context
+    this.audioCtx = wx.createAudioContext('myAudio')
+  },
+  data: {
+    poster: '../../../images/code-cloud-callback-config.png',
+    name: '公交痴汉',
+    author: '变态',
+    src: '../../../video/1.mp4',
+  },
+  audioPlay: function () {
+    this.audioCtx.play()
+  },
+  audioPause: function () {
+    this.audioCtx.pause()
+  },
+  audio14: function () {
+    this.audioCtx.seek(14)
+  },
+  audioStart: function () {
+    this.audioCtx.seek(0)
+  }
+})
---
+++ test1/miniprogram/pages/viewContainer/coverView/coverView.js
@@ -0,0 +1,13 @@
+Page({
+  onShareAppMessage() {
+    return {
+      title: 'cover-view',
+      path: 'pages/viewContainer/coverView/coverView'
+    }
+  },
+
+  data: {
+    latitude: 23.099994,
+    longitude: 113.324520,
+  }
+})
